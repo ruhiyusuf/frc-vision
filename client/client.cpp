7a6eb8ae3fc3ba6baff2d070d2d92d8a8feb9a66
ruhiyusuf <ruhi.yusuf@gmail.com>
2023-01-03 19:31:05 -0800

    added base tag16h5 images

--- client/client.cpp
+++ client/client.cpp
@@ -47,7 +47,8 @@ int main()
 
 	// assign IP, PORT
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_addr.s_addr = inet_addr("10.0.0.18");
+	// servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servaddr.sin_port = htons(PORT);
 
 	// connect the client socket to server socket
